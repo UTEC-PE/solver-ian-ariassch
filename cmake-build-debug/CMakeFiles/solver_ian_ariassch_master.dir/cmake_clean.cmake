d67423c4c39cf59ebfe6189f38cb3bd52d5eaec1
ian-ariassch <38139729+ian-ariassch@users.noreply.github.com>
2018-09-21 19:52:46 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/solver_ian_ariassch_master.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver_ian_ariassch_master.dir/main.cpp.o"
+  "solver_ian_ariassch_master.pdb"
+  "solver_ian_ariassch_master"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver_ian_ariassch_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
